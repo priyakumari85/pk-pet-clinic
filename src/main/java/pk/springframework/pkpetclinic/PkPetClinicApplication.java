36dcac523a034391779512820570c92a78bea07f
priyakumari85 <priya_kumari85@outlook.com>
2020-03-29 20:17:32 -0500

    Initial Project Add

---
+++ src/main/java/pk/springframework/pkpetclinic/PkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pk.springframework.pkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pk/springframework/pkpetclinic/PkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.springframework.pkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
